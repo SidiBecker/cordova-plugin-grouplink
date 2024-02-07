6bf05f771b8fdc80f42f6c4b840e2e6fade4bf11
SidiBecker <sidibecker@hotmail.com>
2024-02-07 18:32:48 -0300

    plugin

--- www/GroupLinkPlugin.js
+++ www/GroupLinkPlugin.js
@@ -1,6 +1,13 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
+var GroupLink = function() {
+};
+
+GroupLink.coolMethod = function (arg0, success, error) {
     exec(success, error, 'GroupLinkPlugin', 'coolMethod', [arg0]);
     window.alert('TESTE');
 };
+
+
+
+exports.GroupLink;
