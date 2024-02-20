c1d1a6da3271ca277aff0d94a2fa1d9a3a5b46a3
SidiBecker <sidibecker@hotmail.com>
2024-02-20 15:32:33 -0300

    plugin

--- www/GroupLinkPlugin.js
+++ www/GroupLinkPlugin.js
@@ -1,76 +1,73 @@
-cordova.define("cordova-plugin-grouplink.groupLink", function (require, exports, module) {
-    var exec = require('cordova/exec');
+var exec = require('cordova/exec');
 
-    var groupLink = function () {
-    };
+var groupLink = function () {
+};
 
-    var ensureBoolean = function (callback) {
-        return function (result) {
+var ensureBoolean = function (callback) {
+    return function (result) {
 
-            if (typeof result == "boolean") {
-                callback(result);
-                return;
-            }
-
-            callback(result == "true");
+        if (typeof result == "boolean") {
+            callback(result);
+            return;
         }
-    };
 
-    var ensureArray = function (callback) {
-        return function (result) {
-
-            if (result == null) {
-                return [];
-            }
-
-            if (Array.isArray(result)) {
-                callback(result);
-                return;
-            }
-
-            callback(JSON.parse(result));
-        }
-    };
-
-    var execMethod = function (method, param, success, error) {
-        exec(success, error, 'GroupLinkPlugin', method, [param]);
-    };
-
-    groupLink.register = function (param, success, error) {
-        execMethod('register', param, success, error);
-    };
-
-    groupLink.requestPermissions = function (param, success, error) {
-        execMethod('requestPermissions', param, success, error);
-    };
-
-    groupLink.getUserId = function (param, success, error) {
-        execMethod('getUserId', param, success, error);
-    };
-
-    groupLink.checkPermissions = function (param, success, error) {
-        execMethod('checkPermissions', param, ensureBoolean(success), error);
-    };
-
-    groupLink.subscribePermissionsStatus = function (param, success, error) {
-        execMethod('subscribePermissionsStatus', param, ensureBoolean(success), error);
-    };
-
-    groupLink.unsubscribePermissionsStatus = function (param, success, error) {
-        execMethod('unsubscribePermissionsStatus', param, success, error);
-    };
-
-    groupLink.getUnauthorizedPermissions = function (param, success, error) {
-        execMethod('getUnauthorizedPermissions', param, ensureArray(success), error);
-    };
-
-
-    if (!window.plugins) {
-        window.plugins = {};
+        callback(result == "true");
     }
+};
 
-    window.plugins.groupLink = groupLink;
+var ensureArray = function (callback) {
+    return function (result) {
 
-    module.exports = groupLink;
+        if (result == null) {
+            return [];
+        }
 
-});
+        if (Array.isArray(result)) {
+            callback(result);
+            return;
+        }
+
+        callback(JSON.parse(result));
+    }
+};
+
+var execMethod = function (method, param, success, error) {
+    exec(success, error, 'GroupLinkPlugin', method, [param]);
+};
+
+groupLink.register = function (param, success, error) {
+    execMethod('register', param, success, error);
+};
+
+groupLink.requestPermissions = function (param, success, error) {
+    execMethod('requestPermissions', param, success, error);
+};
+
+groupLink.getUserId = function (param, success, error) {
+    execMethod('getUserId', param, success, error);
+};
+
+groupLink.checkPermissions = function (param, success, error) {
+    execMethod('checkPermissions', param, ensureBoolean(success), error);
+};
+
+groupLink.subscribePermissionsStatus = function (param, success, error) {
+    execMethod('subscribePermissionsStatus', param, ensureBoolean(success), error);
+};
+
+groupLink.unsubscribePermissionsStatus = function (param, success, error) {
+    execMethod('unsubscribePermissionsStatus', param, success, error);
+};
+
+groupLink.getUnauthorizedPermissions = function (param, success, error) {
+    execMethod('getUnauthorizedPermissions', param, ensureArray(success), error);
+};
+
+
+if (!window.plugins) {
+    window.plugins = {};
+}
+
+window.plugins.groupLink = groupLink;
+
+module.exports = groupLink;
